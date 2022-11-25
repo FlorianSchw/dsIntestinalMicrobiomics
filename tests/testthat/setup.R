d7cd99dddc09323d8033a971a4d6e0c0435307ff
FlorianSchw <Florian.Schwarz@dife.de>
2022-11-25 15:43:52 +0100

    updating test setup + Description file

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,21 +1,9 @@
-# from Newcastle University DataSHIELD Developer Team GitHub page
-
-
-#context("setup - initialization")
-
-
-
-library(factoextra)
-library(cluster)
-library(stats)
-library(NbClust)
+library(betareg)
+library(IFAA)
+library(zoib)
+library(SummarizedExperiment)
 
 
 source("disclosure/set_disclosure_settings.R")
 source("random/set_random_seed_settings.R")
-
-
-
-
-#context("setup - done")
 
